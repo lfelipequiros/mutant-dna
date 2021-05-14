3831398ed492343b71ae97c891c0b27622b82134
Felipe Quirós Cárdenas <felipequiros@Felipes-MacBook-Pro.local>
2021-05-13 21:58:52 -0500

    Enabled Horizontal check

--- server/dna-analysis/index.js
+++ server/dna-analysis/index.js
@@ -2,38 +2,57 @@ const isMutant = (dnaSequence) => {
     return horizontalCheck(dnaSequence);
 };
 
-// Horizontal TEST end match ["actgaaac", "actgaaac","actgaaac","actgaaaa"]
-// Instant break ["aaaactgaaac", "actgaaac","actgaaac","actgaaac"]
+// NOT NXN DONT USE ANYMORE - Horizontal TEST end match ["actgaaac", "actgaaac","actgaaac","actgaaac"]
+// NOT NXN DONT USE ANYMORE - Instant break horizontal ["aaaactgaaac", "actgaaac","actgaaac","actgaaac"]
+// iNOT NXN DONT USE ANYMORE - nstant vertical TEST end match ["actgaaac", "actgaaac","actgaaac","actgaaac"]
+// Vertical TEST end match  4x4 matrix ["aaac", "aaac","aaac","tttc"]
+// Vertical TEST instant match  4x4 matrix ["aaac", "aaac","aaac","attt"]
 const horizontalCheck = (dnaSequence) => {
     const countTrigger = 3;
     let result = false;
 
     let count = 0;
-    //let vCount = 0; //same horizontal logic
+    let vCount = 0; //same horizontal logic
     let currentNitrogenBase = null;
-    //let currentVnitrogenBase = null; //same horizontal logic 
+    let currentVnitrogenBase = null; //same horizontal logic 
 
     for (var j = 0; j < dnaSequence.length; j++) { //loop on array
-        var row = dnaSequence[j];
-        currentNitrogenBase = row[0]; // get first item
-        //currentVnitrogenBase = dnaSequence[0][j]; // TODO will be used to check verticals
-        for (var i = 1; i < row.length; i++) { //loop in the string from 2nd char
-            if(row[i]===currentNitrogenBase){ //check if we have a match and if we do
+
+        currentNitrogenBase = dnaSequence[j][0]; // first Horizontal item
+        currentVnitrogenBase = dnaSequence[0][j]; // first Vertical item
+
+        for (var i = 1; i < dnaSequence[j].length; i++) { //loop in the string from 2nd char
+
+            if(dnaSequence[j][i]===currentNitrogenBase){ //check if we have a match and if we do
                 count++; //start counting
                 if(count>=countTrigger){ // check if we have enough matches to call it a win
                     result = true; // call it a win
-                    console.log("win ", row, i, currentNitrogenBase, count);
+                    console.log("win horizontal", i, currentNitrogenBase, count);
                     break; // break string loop
                 }
             }else{ //if we don't have a match
-                console.log("reset count ", row, i, currentNitrogenBase, count);
+                console.log("reset count horizontal", i, currentNitrogenBase, count);
                 count = 0; // reset count
-                currentNitrogenBase = row[i]; // update nitrogen base we are looking for
+                currentNitrogenBase = dnaSequence[j][i]; // update nitrogen base we are looking for
             }
+
+            if(dnaSequence[i][j]===currentVnitrogenBase){ //check if we have a match and if we do
+                vCount++; //start counting
+                if(vCount>=countTrigger){ // check if we have enough matches to call it a win
+                    result = true; // call it a win
+                    console.log("win vertical", i, currentVnitrogenBase, vCount);
+                    break; // break string loop
+                }
+            }else{ //if we don't have a match
+                console.log("reset count vertical", i, currentVnitrogenBase, vCount);
+                vCount = 0; // reset count
+                currentVnitrogenBase = dnaSequence[i][j]; // update nitrogen base we are looking for
+            }
+
           }
           if (result) break; // if we already have a win break array's foreach
 
-          console.log("fail ", row, i, currentNitrogenBase, count);
+          console.log("fail ");
     };
     return result;
 };
